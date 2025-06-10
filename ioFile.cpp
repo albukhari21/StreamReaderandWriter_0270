cd7b43810a47838af4f0313dd8502b20846abb9e
albukhari21 <albukhariinsankamil@gmail.com>
2025-06-10 11:08:23 +0700

    Add file reading functionality and close file after writing

--- ioFile.cpp
+++ ioFile.cpp
@@ -22,4 +22,13 @@ int main() {
         // menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuka file dalam mode pembaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    
     

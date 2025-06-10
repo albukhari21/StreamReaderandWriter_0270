9f3ad705a84087f4743c74bc13f0c42315304feb
albukhari21 <albukhariinsankamil@gmail.com>
2025-06-10 11:13:41 +0700

    Remove exception handling code from manualException.cpp

--- manualException.cpp
+++ manualException.cpp
@@ -2,20 +2,3 @@
 using namespace std;
 
 int main()
-{
-    try {
-        cout << "Selamat Belajar di Prodi TI UMY" << endl;
-        throw 5; //melemparkan sebuah integer maka
-        cout << "Pernyataan tidak akan dieksekusi" << endl;
-    }
-    catch (int a) {
-        //blok ini akan dieksekusi
-        cout << "Pengecualian akan dieksekusi" << endl;
-    }
-    catch (...) {
-        //*jika selain integer maka block ini akan dieksekusi*/
-        cout << "default Pengecualian dieksekusi" << endl;
-    }
-
-    return 0;
-}

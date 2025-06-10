36568bc552d812140a3df81044c67066d60e4a6d
albukhari21 <albukhariinsankamil@gmail.com>
2025-06-10 11:17:56 +0700

    Remove main function and exception handling code from exceptionStandard.cpp

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -5,24 +5,3 @@
 //untuk array yang akan digunakan
 using namespace std;
 
-int main()
-{
-    cout << "Awa; Program" << endl; // penanda 1:....
-    try {
-        array<int, 3> data = { 10, 20, 30};
-        //int data[3] = { 10, 20, 30 }
-        //pesan array integer dengan 3 elemen
-        cout<<data.at(5) << endl;
-        //memanggil array dengan elemen ke 5
-    }
-    catch (exception& e) {
-        //penangkapan menggunakan objek exception
-        cout << e.what() << endl;
-        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
-
-    }
-    cout << "Baris program yang terakhir" << endl;
-    /*penanda 2: bahwa prgoram berjalan tanpa berhenti meskipun
-    terjadi kesalahan*/
-    return 0;
-}

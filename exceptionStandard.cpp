1ec8e96e870c04e8f8fff1d951d287cb86b7c809
albukhari21 <albukhariinsankamil@gmail.com>
2025-06-10 11:00:31 +0700

    Implement file writing and reading functionality with exception handling demonstration

---
+++ exceptionStandard.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk array yang akan digunakan
+using namespace std;
+
+int main()
+{
+    cout << "Awa; Program" << endl; // penanda 1:....
+    try {
+        array<int, 3> data = { 10, 20, 30};
+        //int data[3] = { 10, 20, 30 }
+        //pesan array integer dengan 3 elemen
+        cout<<data.at(5) << endl;
+        //memanggil array dengan elemen ke 5
+
+    }
+}

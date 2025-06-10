5b4f131ca7d718eb4af99f1142a323ba5a5b4de5
albukhari21 <albukhariinsankamil@gmail.com>
2025-06-10 11:09:31 +0700

    Remove file writing and reading functionality from ioFIleWithPath.cpp

--- ioFIleWithPath.cpp
+++ ioFIleWithPath.cpp
@@ -10,47 +10,4 @@ int main () {
     cout << "Masukkan Nama FIle : ";
     cin >> NamaFile;
 
-    // Membuka file dalam mode menulis.
-    ofstream outfile;
-    // Menunjuk ke sebuah nama file
-    outfile.open(NamaFile + ".txt", ios::out);
-
-    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
-
-    // Unlimited loop untuk menulis
-    while (true) {
-        cout << "- ";
-        //mendapatkan setiap karakter dalam satu baris
-        getline(cin, baris);
-        //loop akan berhenti jika anda memasukkan karakter q
-        if (baris == "q") break;
-        // menulis dan memasukkan nilai dari 'baris' ke dalam file
-        outfile << baris << endl;
-    }
-    //selesai dalam menulis sekarang tutup filenya
-    outfile.close();
-
-
-    //Membuka file dalam mode membaca
-    ifstream infile;
-
-    //Menunjuk ke sebuah file
-    infile.open(NamaFile + ".txt", ios::in);
-
-    cout << endl << ">= Membuka dan membaca file" << endl;
-    //jika file ada maka
-    if (infile.is_open())
-    {
-        //melakukan perulangan setiap baris
-        while (getline(infile, baris))
-        {
-            //dan tampilkan di sini
-            cout << baris << '\n';
-        }
-        //tutup file tersebut setelah selesai
-        infile.close();
-    }
-    //jika tidak ditemukan file maka akan menampilkan ini
-    else cout << "Unable to open file";
-    return 0; 
-}
+    
